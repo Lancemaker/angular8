8cf950da273e4bcf011f045c83286a845202e264
Gil <gil.silva@convergeyou.com.br>
2020-01-31 15:03:05 -0300

    sorry ms jackson uhhhhh

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server created';
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
